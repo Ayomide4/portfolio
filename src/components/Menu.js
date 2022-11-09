e3cc01a92137eec89045cf8f45ce2a14f470cc4f
Ayo Omotosho <aomotosho4@gmail.com>
2022-11-09 15:47:50 -0600

    finished curtain/slide transition for menu + fixed button position

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,19 +9,33 @@ import { HiX } from "react-icons/hi";
 
 export default function Menu(){
     const [open, setOpen] = useState(false)
+    
 
     const toggleNav = () => {
         setOpen(open => !open)
+
+        const menuElement = document.querySelector(".menu-container") //we create the var here so we can store the element after its created
+
+        if(open){
+            menuElement.classList.add("close")
+            menuElement.classList.remove("open")
+        }
+        else{
+            
+            menuElement.classList.add("open")
+            menuElement.classList.remove("close")
+        }
+
         }
 
     return (
 
-            <div className="menu-container">
-                <button className="nav-toggle " type="button" onClick={toggleNav}>
-                    {!open ? <i className="open"><HiMenu/></i> : <></>}
+        <div>
+            <main className="menu-container close"></main>
+            <button className="nav-toggle " type="button" onClick={toggleNav}>
+                    {!open ? <i className="open" ><HiMenu/></i> : <></>}
                     {open ? <i className="close"><HiX/></i> : <></>}
-                </button>
-            </div>
-
+            </button>
+        </div>
     )
 }
