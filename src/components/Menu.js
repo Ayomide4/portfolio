402f02ed432d1744e447d7e4f01768173076b1f8
Ayo Omotosho <aomotosho4@gmail.com>
2022-11-12 00:14:17 -0600

    fix overflow bug

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,36 +10,28 @@ import { HiX } from "react-icons/hi";
 export default function Menu(){
     const [open, setOpen] = useState(false)
     
-
     const toggleMenu = () => {
         setOpen(open => !open)
-
         const menuElement = document.querySelector(".menu-container") //we create the var here so we can store the element after its created
-
 
         if(open){
             menuElement.classList.add("close")
             menuElement.classList.remove("open")
         }
         else{
-            
             menuElement.classList.add("open")
             menuElement.classList.remove("close")
         }
-
     }
 
     return (
-
         <div>
             <main className="menu-container close"></main>
             <button className="nav-toggle " type="button" onClick={toggleMenu}>
                     {!open ? <i className="open" ><HiMenu/></i> : <></>}
                     {open ? <i className="close"><HiX/></i> : <></>}
             </button>
-            
             <Navbar open={open} toggleMenu={() => toggleMenu}/>
-
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,32 @@ import React from "react";
 export default function Navbar(props){
 
     return (
-        <div className='navbar-container close' >
+        <div className='nav-container close' >
             <nav className={props.open ? "nav-links open" : "nav-links close"}>
+                <div className="nav-item">
+                    <a href="/#">
+                        <h2 className="nav-item-title">Home</h2>
+                        <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
+                    </a>
+                </div>
+                <div className="nav-item">
+                    <a className="nav-link-item" href="/#">
+                        <h2 className="nav-item-title">Projects</h2>
+                        <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
+                    </a>
+                </div>
+                <div className="nav-item">
+                    <a className="nav-link-item" href="/#">
+                        <h2 className="nav-item-title">About Me</h2>
+                        <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
+                    </a>
+                </div>
+                <div className="nav-item">
                 <a className="nav-link-item" href="/#">
-                    <h2>Home</h2>
+                    <h2 className="nav-item-title">Contact Me</h2>
                     <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
                 </a>
-                <a className="nav-link-item" href="/#">
-                    <h2>Projects</h2>
-                    <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
-                </a>
-                <a className="nav-link-item" href="/#">
-                    <h2>About Me</h2>
-                    <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
-                </a>
-                <a className="nav-link-item" href="/#">
-                    <h2>Contact Me</h2>
-                    <img className="nav-link-img" src="https://images.pexels.com/photos/14344661/pexels-photo-14344661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
-                </a>
+                </div>
             </nav>
         </div>
     )
